c7cfe6a2da61053af5e7047e51e9dd3b740e9a63
Raanan Weber <raananw+github@gmail.com>
2015-03-30 22:48:00 +0200

    Added geometries and cleanup
    Database is now used to separately store geometries and meshes, to boost
    performance in mesh updates.

--- src/IndexedDBPersist.ts
+++ src/IndexedDBPersist.ts
@@ -10,8 +10,7 @@ module BABYLONX {
         id: string;
         name: string;
         uniqueId: number;
-        indices: Array<number>;
-        positions: Array<number>;
+        geometryId: string;
         sphereCenter: Array<number>;
         sphereRadius: number;
         boxMinimum: Array<number>;
@@ -29,7 +28,15 @@ module BABYLONX {
         indexCount: number;
     }
 
-    export class MeshSerialization {
+    export interface SerializedGeometry {
+        id: string;
+        positions: Array<number>;
+        indices: Array<number>;
+        normals: Array<number>;
+        uvs?: Array<number>;
+    }
+
+    export class BabylonSerialization {
         public static SerializeMesh = function (mesh: BABYLON.AbstractMesh) : SerializedMesh {
             var submeshes = [];
             if (mesh.subMeshes) {
@@ -43,13 +50,14 @@ module BABYLONX {
                     }
                 });
             }
+
+            var geometryId = (<BABYLON.Mesh>mesh).geometry ? (<BABYLON.Mesh>mesh).geometry.id : null;
             
             return {
-                uniqueId: <number> mesh['uniqueId'],
+                uniqueId: mesh.uniqueId,
                 id: mesh.id,
                 name: mesh.name,
-                indices: mesh.getIndices(),
-                positions: mesh.getVerticesData(BABYLON.VertexBuffer.PositionKind),
+                geometryId: geometryId,
                 sphereCenter: mesh.getBoundingInfo().boundingSphere.centerWorld.asArray(),
                 sphereRadius: mesh.getBoundingInfo().boundingSphere.radiusWorld,
                 boxMinimum: mesh.getBoundingInfo().boundingBox.minimumWorld.asArray(),
@@ -59,88 +67,130 @@ module BABYLONX {
                 checkCollisions: mesh.checkCollisions
             }
         }
+
+        public static SerializeGeometry = function (geometry: BABYLON.Geometry): SerializedGeometry {
+            return {
+                id: geometry.id,
+                positions: geometry.getVerticesData(BABYLON.VertexBuffer.PositionKind),
+                normals: geometry.getVerticesData(BABYLON.VertexBuffer.NormalKind),
+                indices: geometry.getIndices(),
+                uvs: geometry.getVerticesData(BABYLON.VertexBuffer.UVKind)
+            }
+        }
     }
 
     export class IndexedDBPersist {
 
-        private indexedDb_: IDBDatabase;
+        private _indexedDb: IDBDatabase;
 
-        public static OBJECT_STORE_NAME: string = "meshes";
+        public static MESHES_OBJECT_STORE_NAME: string = "meshes";
+        public static GEOMETRIES_OBJECT_STORE_NAME: string = "geometries";
 
         //to prevent too many locks
-        private addUpdateList: { [n:number]: SerializedMesh; }//: Array<SerializedMesh>;
-        private remvoeList: Array<number>;
-
-        //temp
-        private uniqueIdCounter_ = 0;
-
-        public onDatabaseUpdated: (updatedKeys: Array<number>) => void;
+        private _addUpdateList: { [n: number]: SerializedMesh; }//: Array<SerializedMesh>;
+        private _addUpdateListGeometries: { [s: string]: SerializedGeometry; };
+        private _remvoeList: Array<number>;
+        private _removeListGeometries: Array<string>;
+        
+        public onDatabaseUpdated: (updatedMeshesUniqueIds: Array<number>, updatedGeometriesIds: Array<string>) => void;
 
         /**
          * @param _scene {BABYLON.Scene} - the BabylonJS scene to be used.
          * @param processRegistered {boolean} - should already-registered nodes be processed by the extension. defaults to true.
          */
         constructor(private _scene: BABYLON.Scene, dbName: string = "babylonJsMeshes", processRegistered: boolean = true) {
-            this.addUpdateList = {};
-            this.remvoeList = [];
+            this._addUpdateList = {};
+            this._addUpdateListGeometries = {};
+            this._remvoeList = [];
+            this._removeListGeometries = [];
+            
 
-            this.openDatabase(dbName, 1, true,(db: IDBDatabase) => {
-                this.indexedDb_ = db;
-                this._scene['onNewMeshAdded'] = this.onMeshAdded;
-                this._scene['onMeshRemoved'] = this.onMeshRemoved;
-                this._scene.registerAfterRender(this.processLists);
+            this._openDatabase(dbName, 1, true,(db: IDBDatabase) => {
+                this._indexedDb = db;
+                this._scene.onNewMeshAdded = this._onMeshAdded;
+                this._scene.onMeshRemoved = this._onMeshRemoved;
+                this._scene.onGeometryAdded = this._onGeometryAdded;
+                this._scene.onGeometryRemoved = this._onGeometryRemoved;
+                this._scene.registerAfterRender(this._processLists);
                 if (processRegistered) {
-                    //register already-created meshes
+                    //register already-created meshes and geometries
                     setTimeout(() => {
-                        this._scene.meshes.forEach((node, index) => {
-                            this.onMeshAdded(node, index);
+                        this._scene.meshes.forEach((node) => {
+                            this._onMeshAdded(node);
+                        });
+                        this._scene.getGeometries().forEach((geometry) => {
+                            this._onGeometryAdded(geometry);
                         });
                     });
                 }
             });
-            
+             
         }
 
-        private onMeshAdded = (mesh: BABYLON.AbstractMesh, position: number) => {
-            //Unneeded with the newest BJS version
-            //mesh['uniqueId'] = this.uniqueIdCounter_++; 
-            mesh.registerAfterWorldMatrixUpdate(this.onMeshUpdated);
-            this.addUpdateList[<number> mesh['uniqueId']] = MeshSerialization.SerializeMesh(mesh);
+        private _onMeshAdded = (mesh: BABYLON.AbstractMesh) => {
+            mesh.registerAfterWorldMatrixUpdate(this._onMeshUpdated);
+            this._addUpdateList[mesh.uniqueId] = BabylonSerialization.SerializeMesh(mesh);
         }
 
-        private onMeshRemoved = (mesh: BABYLON.AbstractMesh) => {
-            this.remvoeList.push(mesh['uniqueId']);
+        private _onMeshRemoved = (mesh: BABYLON.AbstractMesh) => {
+            this._remvoeList.push(mesh.uniqueId);
         }
 
-        private onMeshUpdated = (mesh: BABYLON.AbstractMesh) => {
-            this.addUpdateList[<number> mesh['uniqueId']] = MeshSerialization.SerializeMesh(mesh);
+        private _onMeshUpdated = (mesh: BABYLON.AbstractMesh) => {
+            this._addUpdateList[mesh.uniqueId] = BabylonSerialization.SerializeMesh(mesh);
         }
 
-        private processLists = () => {
-            if (!this.indexedDb_) return;
-            var updated: Array<number> = [];
-            for (var property in this.addUpdateList) {
-                if (this.addUpdateList.hasOwnProperty(property)) {
-                    updated.push(parseInt(property));
-                    this.processMeshAddedUpdated(this.addUpdateList[property]);
-                    delete this.addUpdateList[property];
+        private _onGeometryAdded = (geometry: BABYLON.Geometry) => {
+            geometry.onGeometryUpdated = this._onGeometryUpdated;
+            this._addUpdateListGeometries[geometry.id] = BabylonSerialization.SerializeGeometry(geometry);
+        }
+
+        private _onGeometryRemoved = (geometry: BABYLON.Geometry) => {
+            this._removeListGeometries.push(geometry.id);
+        }
+
+        private _onGeometryUpdated = (geometry: BABYLON.Geometry) => {
+            this._addUpdateListGeometries[geometry.id] = BabylonSerialization.SerializeGeometry(geometry);
+        }
+
+        private _processLists = () => {
+            if (!this._indexedDb) return;
+            var updatedMeshes: Array<number> = [];
+            var updatedGeometries: Array<string> = [];
+            for (var uniqueId in this._addUpdateList) {
+                if (this._addUpdateList.hasOwnProperty(uniqueId)) {
+                    updatedMeshes.push(parseInt(uniqueId));
+                    this._processMeshAddedUpdated(this._addUpdateList[uniqueId]);
+                    delete this._addUpdateList[uniqueId];
                 }
             }
-            while (this.remvoeList.length) {
-                var toRemove = this.remvoeList.pop()
-                updated.push(toRemove);
-                this.processMeshRemoved(toRemove);
+            for (var id in this._addUpdateListGeometries) {
+                if (this._addUpdateListGeometries.hasOwnProperty(id)) {
+                    updatedGeometries.push(id);
+                    this.processGeometryAddedUpdated(this._addUpdateListGeometries[id]);
+                    delete this._addUpdateListGeometries[id];
+                }
             }
-            if (updated.length) {
+            while (this._remvoeList.length) {
+                var toRemove = this._remvoeList.pop()
+                updatedMeshes.push(toRemove);
+                this._processMeshRemoved(toRemove);
+            }
+            while (this._removeListGeometries.length) {
+                var gToRemove = this._removeListGeometries.pop()
+                updatedGeometries.push(gToRemove);
+                this._processGeometryRemoved(gToRemove);
+            }
+            if (updatedMeshes.length || updatedGeometries.length) {
                 if (this.onDatabaseUpdated) {
-                    this.onDatabaseUpdated(updated);
+                    this.onDatabaseUpdated(updatedMeshes, updatedGeometries);
                 }
             }
         }
 
-        private processMeshAddedUpdated = (serializedMesh: SerializedMesh) => {
+        private _processMeshAddedUpdated = (serializedMesh: SerializedMesh) => {
             
-            var transaction = this.indexedDb_.transaction([IndexedDBPersist.OBJECT_STORE_NAME], "readwrite");
+            var transaction = this._indexedDb.transaction([IndexedDBPersist.MESHES_OBJECT_STORE_NAME], "readwrite");
             transaction.oncomplete = function (event) {
                 //console.debug("Adding done,", serializedMesh.name);
             };
@@ -149,13 +199,13 @@ module BABYLONX {
                 console.log(event);
             };
 
-            var objectStore = transaction.objectStore(IndexedDBPersist.OBJECT_STORE_NAME);
+            var objectStore = transaction.objectStore(IndexedDBPersist.MESHES_OBJECT_STORE_NAME);
             //todo handle the request events
             objectStore.put(serializedMesh, serializedMesh.uniqueId);
         }
 
-        private processMeshRemoved = (uniqueId: number) => {
-            var transaction = this.indexedDb_.transaction([IndexedDBPersist.OBJECT_STORE_NAME], "readwrite");
+        private _processMeshRemoved = (uniqueId: number) => {
+            var transaction = this._indexedDb.transaction([IndexedDBPersist.MESHES_OBJECT_STORE_NAME], "readwrite");
             transaction.oncomplete = function (event) {
             };
 
@@ -163,12 +213,42 @@ module BABYLONX {
                 console.log(event);
             };
 
-            var objectStore = transaction.objectStore(IndexedDBPersist.OBJECT_STORE_NAME);
+            var objectStore = transaction.objectStore(IndexedDBPersist.MESHES_OBJECT_STORE_NAME);
             //todo handle the request events
             objectStore.delete(uniqueId);
         }
 
-        private openDatabase(dbName: string, dbVersion: number, deleteDatabase: boolean, successCallback: (db: IDBDatabase) => void) {
+        private processGeometryAddedUpdated = (serializedGeometry: SerializedGeometry) => {
+
+            var transaction = this._indexedDb.transaction([IndexedDBPersist.GEOMETRIES_OBJECT_STORE_NAME], "readwrite");
+            transaction.oncomplete = function (event) {
+                //console.debug("Adding done,", serializedMesh.name);
+            };
+
+            transaction.onerror = function (event) {
+                console.log(event);
+            };
+
+            var objectStore = transaction.objectStore(IndexedDBPersist.GEOMETRIES_OBJECT_STORE_NAME);
+            //todo handle the request events
+            objectStore.put(serializedGeometry, serializedGeometry.id);
+        }
+
+        private _processGeometryRemoved = (id: string) => {
+            var transaction = this._indexedDb.transaction([IndexedDBPersist.GEOMETRIES_OBJECT_STORE_NAME], "readwrite");
+            transaction.oncomplete = function (event) {
+            };
+
+            transaction.onerror = function (event) {
+                console.log(event);
+            };
+
+            var objectStore = transaction.objectStore(IndexedDBPersist.GEOMETRIES_OBJECT_STORE_NAME);
+            //todo handle the request events
+            objectStore.delete(id);
+        }
+
+        private _openDatabase(dbName: string, dbVersion: number, deleteDatabase: boolean, successCallback: (db: IDBDatabase) => void) {
             if (deleteDatabase) {
                 indexedDB.deleteDatabase(dbName);
             }
@@ -185,23 +265,23 @@ module BABYLONX {
 
             request.onupgradeneeded = function (event: IDBVersionChangeEvent) {
                 var openedDb = event.target['result'];
-                var meshesObjectStore = openedDb.createObjectStore(IndexedDBPersist.OBJECT_STORE_NAME/*, { autoIncrement: true }*/);
-                meshesObjectStore.createIndex("uniqueId", "uniqueId", { unique: true });  
-                meshesObjectStore.createIndex("name", "name", { unique: false }); 
-                meshesObjectStore.createIndex("id", "id", { unique: false });
+                var meshesObjectStore = openedDb.createObjectStore(IndexedDBPersist.MESHES_OBJECT_STORE_NAME);
+                meshesObjectStore.createIndex("uniqueId", "uniqueId", { unique: true });
+                var geometriesObjectStore = openedDb.createObjectStore(IndexedDBPersist.GEOMETRIES_OBJECT_STORE_NAME);
+                geometriesObjectStore.createIndex("id", "id", { unique: true });
             }
 
         }
 
         public countMeshes(countCallback: (count: number) => void): void {
-            if (!this.indexedDb_) return;
-            var transaction = this.indexedDb_.transaction([IndexedDBPersist.OBJECT_STORE_NAME], "readonly");
+            if (!this._indexedDb) return;
+            var transaction = this._indexedDb.transaction([IndexedDBPersist.MESHES_OBJECT_STORE_NAME], "readonly");
 
             transaction.onerror = function (event) {
                 console.log(event);
             };
 
-            var objectStore = transaction.objectStore(IndexedDBPersist.OBJECT_STORE_NAME);
+            var objectStore = transaction.objectStore(IndexedDBPersist.MESHES_OBJECT_STORE_NAME);
             var index = objectStore.index("uniqueId");
             var req = index.count();
             req.onsuccess = function (event) {
@@ -210,8 +290,8 @@ module BABYLONX {
         }
 
         public getAllMeshes(callback: (meshes:Array<SerializedMesh>) => void) {
-            var trans = this.indexedDb_.transaction([IndexedDBPersist.OBJECT_STORE_NAME]);
-            var store = trans.objectStore(IndexedDBPersist.OBJECT_STORE_NAME);
+            var trans = this._indexedDb.transaction([IndexedDBPersist.MESHES_OBJECT_STORE_NAME]);
+            var store = trans.objectStore(IndexedDBPersist.MESHES_OBJECT_STORE_NAME);
             var meshes = [];
 
             trans.oncomplete = function (evt) {
